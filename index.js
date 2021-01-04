682fccb598c53ae6c1cb22ac573327dd9caa08e9
John Schroeder <john@schroederspace.com>
2021-01-03 22:51:42 -0500

    motion

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const scale = 2;
 canvas.width = map.width * map.tilewidth * scale;
 canvas.height = map.height * map.tileheight * scale;
 
+// TILES
+
 const loadImages = (callback) => {
     let imagesLoaded = 0;
     const onImageLoaded = () => {
@@ -55,8 +57,10 @@ const drawTile = (tile, row, col) => {
     );
 }
 
+const getOffset = (layer, row, col) => (row * layer.width) + col;
+
 const drawLayerTile = (layer, row, col) => {
-    const offset = (row * layer.width) + col;
+    const offset = getOffset(layer, row, col);
     const gid = layer.data[offset];
     if (gid == 0)
         return;
@@ -80,68 +84,82 @@ const renderTile = (row, col) => {
     }
 }
 
-let heroDown = null;
-let heroLeft = null;
-let heroRight = null;
-let heroUp = null;
+const isCollision = (row, col) => {
+    // anything after the first 2 layers is a collision
+    for(let i = 2; i < map.layers.length; i++) {
+        const layer = map.layers[i];
+        const offset = getOffset(layer, row, col);
+        if (layer.data[offset] > 0)
+            return true;
+    }
+    return false;
+}
+
+// MOTION
+
+let hero = {};
 
 let heroTile = null;
 let heroRow = 11;
 let heroCol = 0;
 
-const moveHero = (row, col) => {
+const getCharacterDirection = (startid) => {
+    return {
+        walk1: getTile(startid),
+        still: getTile(startid + 1),
+        walk2: getTile(startid + 2)
+    }
+}
+
+const getCharacter = (startid) => {
+    return {
+        down: getCharacterDirection(startid),
+        left: getCharacterDirection(startid + 12),
+        right: getCharacterDirection(startid + 24),
+        up: getCharacterDirection(startid + 36)
+    }
+}
+
+const changeHero = (tile, row, col) => {
     renderTile(heroRow, heroCol);
+    heroTile = tile;
     heroRow = row;
     heroCol = col;
     drawTile(heroTile, heroRow, heroCol);
-};
-
-const changeHero = (tile) => {
-    heroTile = tile;
-    drawTile(heroTile, heroRow, heroCol);
 }
 
-loadImages(() => {
-    renderMap();
-    heroDown = getTile(125);
-    heroLeft = getTile(137);
-    heroRight = getTile(149);
-    heroUp = getTile(161);
-    heroTile = heroRight;
-    drawTile(heroTile, heroRow, heroCol);
-});
+const getNextTile = (dir) => {
+    switch(heroTile) {
+        case dir.still: return dir.walk1;
+        case dir.walk1: return dir.walk2;
+        case dir.walk2: return dir.walk1;
+        default: return dir.still;
+    }
+}
 
-const right = () => {
-    if (heroTile == heroRight)
-        moveHero(heroRow, heroCol + 1);
+const heroMotion = (dir, row, col) => {
+    const next = getNextTile(dir);
+    if (next == dir.still || row < 0 || col < 0 || row >= map.height || col >= map.width || isCollision(row, col))
+        changeHero(dir.still, heroRow, heroCol);
     else
-        changeHero(heroRight);
-};
-const left = () => {
-    if (heroTile == heroLeft)
-        moveHero(heroRow, heroCol - 1);
-    else
-        changeHero(heroLeft);
-};
-const up = () => {
-    if (heroTile == heroUp)
-        moveHero(heroRow - 1, heroCol);
-    else
-        changeHero(heroUp);
-};
-const down = () => {
-    if (heroTile == heroDown)
-        moveHero(heroRow + 1, heroCol);
-    else
-        changeHero(heroDown);
+        changeHero(next, row, col);
 };
 
 document.addEventListener('keydown', (e) => {
     switch(e.key) {
-        case 'ArrowRight': return right();
-        case 'ArrowLeft': return left();
-        case 'ArrowUp': return up();
-        case 'ArrowDown': return down();
+        case 'ArrowRight': return heroMotion(hero.right, heroRow, heroCol + 1);
+        case 'ArrowLeft':  return heroMotion(hero.left, heroRow, heroCol - 1);
+        case 'ArrowUp':    return heroMotion(hero.up, heroRow - 1, heroCol);
+        case 'ArrowDown':  return heroMotion(hero.down, heroRow + 1, heroCol);
     }
+});
+
+// INIT
+
+loadImages(() => {
+    renderMap();
+    hero = getCharacter(124);
+    heroTile = hero.right.still;
+    drawTile(heroTile, heroRow, heroCol);
 });
 
